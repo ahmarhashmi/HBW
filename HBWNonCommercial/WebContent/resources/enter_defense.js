9838d53ce52a5d6aca7f251a3733c5c77df9d50f
unknown <ahmar7115@gmail.com>
2017-12-11 21:20:11 +0500

    Remove file and timeout

--- HBWNonCommercial/WebContent/resources/enter_defense.js
+++ HBWNonCommercial/WebContent/resources/enter_defense.js
@@ -23,29 +23,31 @@ Dropzone.options.fileUploadForm = {
 		this.on('maxfilesexceeded', function(file) {
 			this.removeFile(file);
 		});
+		
+		this.on('complete', async function(file){
+			await sleep(15000);
+			$('a.dz-remove').remove();
+		});
+		
+		/** This function is triggered when user attempts to delete a file */
+		this.on('removedfile', async function(file){
+			var CONTEXT_PATH = window.location.pathname.substring(0, window.location.pathname.indexOf("/",2));
+			
+			//new Promise(resolve => setTimeout(resolve, ms));
+			$.get(CONTEXT_PATH + "/FileUploadServlet?delete="+file.name, function(data) {});
+		});
 
-		var submitButton = document.querySelector("#submit-all");
 		var wrapperThis = this;
 
-		/*
-		 * submitButton.addEventListener("click", function() {
-		 * wrapperThis.processQueue(); });
-		 */
-
 		this.on("addedfile", function(file) {
-			// alert(file.type=='application/pdf');
-
 			var isTypeAllowed = file.type.match(/image.*/)
-					|| file.type === 'application/pdf'
+					|| file.type === 'application/pdf';
 
 			if (!isTypeAllowed) {
 				wrapperThis.removeFile(file);
 				alert("File type not supported");
 			}
-			var src = document.getElementsByTagName("img").src;
 			file.previewElement.addEventListener("click", function() {
-				// alert(file.previewElement);
-
 				$("#dialog").innerHTML = file.previewElement;
 				$("#dialog").dialog(
 						{
@@ -62,57 +64,19 @@ Dropzone.options.fileUploadForm = {
 								duration : 800
 							},
 							open : function(event, ui) {
-								// $('#divInDialog').load('test.html',
-								// function() {
-								// alert('Load was performed.');
-								// });
-//								$(document).appendChild("<span id=''></span>")
 								var CONTEXT_PATH = window.location.pathname.substring(0, window.location.pathname.indexOf("/",2));
 								$("#image").attr('src', CONTEXT_PATH + "/FileUploadServlet?file="+file.name);
-								
-								/*$.get(CONTEXT_PATH + "/FileUploadServlet?file="+file.name,
-										function(data) {
-											$('#dialog').html("<img src="+data+" />");
-											$('#dialog').dialog('open');
-										}, "html");*/
 							},
-							title : "Preview of the uploaded file",
+							title : file.name,
 							buttons : [ {
 								text : "Ok",
 								icon : "ui-icon-heart",
 								click : function() {
 									$(this).dialog("close");
 								}
-
-							// Uncommenting the following line would hide the
-							// text,
-							// resulting in the label being used as a tooltip
-							// showText: false
 							} ]
 						});
-				// var win = window.open(src, '_blank');
-				// win.focus();
 			});
-
-			// Create the remove button
-			/*
-			 * var removeButton = Dropzone .createElement("<button class='btn
-			 * btn-lg dark'>Remove File</button>");
-			 */
-			// Listen to the click event
-			/*
-			 * removeButton .addEventListener( "click", function(e) { // Make
-			 * sure the button click doesn't submit the form: e
-			 * .preventDefault(); e .stopPropagation(); // Remove the file
-			 * preview. wrapperThis .removeFile(file); // If you want to the
-			 * delete the file on the server as well, // you can do the AJAX
-			 * request here. });
-			 */
-
-			// Add the button to the file preview element.
-			/*
-			 * file.previewElement .appendChild(removeButton);
-			 */
 		});
 
 		this.on('sendingmultiple', function(data, xhr, formData) {
@@ -120,3 +84,7 @@ Dropzone.options.fileUploadForm = {
 		});
 	}
 };
+
+function sleep(ms) {
+	  return new Promise(resolve => setTimeout(resolve, ms));
+	}
--- HBWNonCommercial/src/hbw/controller/hearing/request/action/FileUploadServlet.java
+++ HBWNonCommercial/src/hbw/controller/hearing/request/action/FileUploadServlet.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -51,46 +50,18 @@ public class FileUploadServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
-	//response.getWriter().append("Served at: ").append(request.getContextPath());
-	
+
 	String requestedFileName = request.getParameter("file");
-	File folder = FileUtil.validateAndGetEvidenceUploadPath(request);
-//	ServletOutputStream out = response.getOutputStream();
-	   // Get the absolute path of the image
-	       ServletContext sc = getServletContext();
-	       String filename = folder.getPath()+File.separator+requestedFileName;// sc.getRealPath("image.gif");
-	    
-	       // Get the MIME type of the image
-	       String mimeType = sc.getMimeType(filename);
-	       if (mimeType == null) {
-	           sc.log("Could not get MIME type of "+filename);
-	           response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-	           return;
-	       }
-	    
-	       // Set content type
-	       response.setContentType(mimeType);
-	    
-	       // Set content size
-	       File file = new File(filename);
-	       response.setContentLength((int)file.length());
-	    try {
-	       // Open the file and output streams
-	       FileInputStream in = new FileInputStream(file);
-	       OutputStream out = response.getOutputStream();
-	    
-	       // Copy the contents of the file to the output stream
-	       byte[] buf = new byte[1024];
-	       int count = 0;
-	       while ((count = in.read(buf)) >= 0) {
-	           out.write(buf, 0, count);
-	       }
-	       in.close();
-	       out.close();
-	    }catch(IOException e) {
-		e.printStackTrace();
+	if (requestedFileName != null) {
+	    doGetFile(request, response, requestedFileName);
+	} else {
+	    String deleteFile = request.getParameter("delete");
+	    if (deleteFile != null) {
+		doDeleteFile(request, response, deleteFile);
+		response.getWriter().append("FileDeleted: ").append(deleteFile);
 	    }
-	   
+	}
+
     }
 
     /**
@@ -133,7 +104,7 @@ public class FileUploadServlet extends HttpServlet {
 		    }
 		    item.write(file);
 
-		    LOGGER.info(file.getPath()+"{} File uploaded SUCCESSFULLY", file.getPath());
+		    LOGGER.info(file.getPath() + "{} File uploaded SUCCESSFULLY", file.getPath());
 		}
 	    }
 	} catch (FileUploadException e) {
@@ -146,4 +117,69 @@ public class FileUploadServlet extends HttpServlet {
 	    writer.close();
 	}
     }
+
+    /**
+     * This function is responsible for getting the file and writing to the
+     * response.
+     * 
+     * @param request
+     * @param response
+     * @param requestedFileName
+     */
+    private void doGetFile(HttpServletRequest request, HttpServletResponse response, String requestedFileName) {
+	File folder = FileUtil.validateAndGetEvidenceUploadPath(request);
+	// Get the absolute path of the image
+	ServletContext sc = getServletContext();
+	String fileName = folder.getPath() + File.separator + requestedFileName;
+
+	// Get the MIME type of the image
+	String mimeType = sc.getMimeType(fileName);
+	if (mimeType == null) {
+	    sc.log("Could not get MIME type of " + fileName);
+	    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	    return;
+	}
+
+	LOGGER.info("File {} mime type is:{}", fileName, mimeType);
+
+	// Set content type
+	response.setContentType(mimeType);
+
+	// Set content size
+	File file = new File(fileName);
+	response.setContentLength((int) file.length());
+	try {
+	    // Open the file and output streams
+	    FileInputStream in = new FileInputStream(file);
+	    OutputStream out = response.getOutputStream();
+
+	    // Copy the contents of the file to the output stream
+	    byte[] buf = new byte[1024];
+	    int count = 0;
+	    while ((count = in.read(buf)) >= 0) {
+		out.write(buf, 0, count);
+	    }
+	    in.close();
+	    out.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+    }
+
+    /**
+     * This function is responsible for deleting the file from the server.
+     * 
+     * @param request
+     * @param response
+     * @param deleteFile
+     */
+    private void doDeleteFile(HttpServletRequest request, HttpServletResponse response, String deleteFile) {
+	File folder = FileUtil.validateAndGetEvidenceUploadPath(request);
+	File fileName = new File(folder.getPath() + File.separator + deleteFile);
+	LOGGER.info("DELETE file:{}", fileName.getPath());
+	fileName.delete();
+	LOGGER.info("file:{} >>> Deleted successfully.", fileName.getPath());
+    }
+
 }
